c0786f0f9943b265c6742f63b908c4d2f16a84aa
Zyaire <Zyaire@gmail.com>
2019-05-30 16:22:45 +0800

    注册页面更新

--- app/src/main/java/com/example/administrator/qyue/RegistActivity.java
+++ app/src/main/java/com/example/administrator/qyue/RegistActivity.java
@@ -3,7 +3,7 @@ package com.example.administrator.qyue;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegistActivity extends AppCompatActivity {
+public class   RegistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
