aec2c2d0158ff9a85f561456489f4424d708ae7d
Zyaire <Zyaire@gmail.com>
2019-06-03 14:56:06 +0800

    把登录页面的bar去除

--- app/src/main/java/com/example/administrator/qyue/LoginActivity.java
+++ app/src/main/java/com/example/administrator/qyue/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.administrator.qyue;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
