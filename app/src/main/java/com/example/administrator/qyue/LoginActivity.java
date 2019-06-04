ca516216ad784a3d2f208f162802f6fc424e1df1
Administrator <1554469782@qq.com>
2019-06-04 11:03:00 +0800

    更新了登录页面

--- app/src/main/java/com/example/administrator/qyue/LoginActivity.java
+++ app/src/main/java/com/example/administrator/qyue/LoginActivity.java
@@ -3,23 +3,40 @@ package com.example.administrator.qyue;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-    private Button login,regist,forget;
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    private String userName, psw;
+    private EditText userTest, pwdTest;
+    private Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button regist=findViewById(R.id.registButtom);
-        Button forget=findViewById(R.id.forgetButtom);
-        Button login=findViewById(R.id.loginButtom);
+        login=findViewById(R.id.loginButtom);
 
-        forget.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,MajorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        init();
+
+    }
+        /*forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(LoginActivity.this,ForgetActivity.class);
@@ -32,16 +49,32 @@ public class LoginActivity extends Activity {
                 Intent intent=new Intent(LoginActivity.this,RegistActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
-        login.setOnClickListener(new View.OnClickListener() {
+
+
+    private void init() {
+        TextView tv_register = (TextView) findViewById(R.id.registButtom);
+        TextView tv_find_psw = (TextView) findViewById(R.id.forgetButtom);
+        Button btn_login = (Button) findViewById(R.id.loginButtom);
+        userTest = (EditText) findViewById(R.id.userTest);
+        pwdTest = (EditText) findViewById(R.id.pwdTest);
+        //立即注册控件的点击事件
+        tv_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(LoginActivity.this,MajorActivity.class);
-                startActivity(intent);
+                //为了跳转到注册界面，并实现注册功能
+                Intent intent = new Intent(LoginActivity.this, RegistActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
+        //找回密码控件的点击事件
+        tv_find_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgetActivity.class));
+            }
+        });
+
     }
-
-
 }
