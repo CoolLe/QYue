4a55a13b98b0132a250427531001b71b67e84026
Administrator <1554469782@qq.com>
2019-06-11 09:04:18 +0800

    更新了添加好友界面页面

---
+++ app/src/main/java/com/example/administrator/qyue/AddFriendActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.qyue;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AddFriendActivity extends BaseActivity implements View.OnClickListener {
+
+
+    private String TAG = "friend";
+    private SearchView mSvSearchFriends;
+    private LinearLayout mLlReturn;
+    private TextView mTvResult;
+
+    private LoadingUtils loadingUtils;
+
+    final ArrayList<Map<String, Object>> listdata = new ArrayList<>();
+    //private Gson gson;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
